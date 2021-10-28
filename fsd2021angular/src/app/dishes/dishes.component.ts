f59f549d583d60e327f3f52ed2db377fc880be99
Mokshi Gupta <mokshigupta0281@gmail.com>
2021-10-28 14:52:12 +0530

    Dishes Added to Firebase

--- fsd2021angular/src/app/dishes/dishes.component.ts
+++ fsd2021angular/src/app/dishes/dishes.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Restaurant } from '../model/restaurant';
+import { RestaurantsService } from '../restaurants.service';
+import { getFirestore, collection, addDoc, getDocs, doc, Timestamp, deleteDoc, setDoc} from '@firebase/firestore/lite'
+import { getStorage, ref, uploadBytes, getDownloadURL } from '@firebase/storage'
+import { DBService } from '../db.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,17 +13,75 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./dishes.component.css']
 })
 export class DishesComponent implements OnInit {
+  dishList : any;
+  dishForm = new FormGroup(
+    {
+      name: new FormControl(''),
+      category: new FormControl(''),
+      price: new FormControl('')
+    }
+  );
 
-  constructor(private route: ActivatedRoute) { }
+  tempImageFile: any;
+
+  constructor(private db: DBService, private route: ActivatedRoute) {  }
 
   restaurantId: String = "";
   restaurantName: String = "";
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
+      // const sessionData = sessionStorage.getItem("restaurant/dishes"+"/dishes");
       this.restaurantId = params['id'];
       this.restaurantName = params['name'];
+      this.fetchDishes();
     });
+  }
+
+  async fetchDishes(){
+    let firestoreDB = getFirestore(this.db.app);
+    let promoCodeCollection = collection(firestoreDB, `restaurants/${this.restaurantId}/dishes`);
+    let snapshots = await getDocs(promoCodeCollection);
+    
+    this.dishList = snapshots.docs.map(
+      doc => {
+        const data = doc.data();
+        data['docId'] = doc.id;
+        return data;
+      }
+    );
+     
+    console.log(this.dishList);
+  }
+
+  async addDishToFirebase(){
+    let values = { ...this.dishForm.value };
+    let firestoreDB = getFirestore(this.db.app);
+    let collectionRef = collection(firestoreDB, `restaurants/${this.restaurantId}/dishes`);
+
+    if(this.tempImageFile !== null) {
+      let storageDB = getStorage(this.db.app);
+      let FileRef = ref(storageDB, `dish-images/${this.tempImageFile.name}`);
+      await uploadBytes(FileRef, this.tempImageFile);
+      values['imageUrl'] = (await getDownloadURL(FileRef)).toString();
+    
+    }
+
+    addDoc(collectionRef, { ...values }).then((value) => {
+      console.log(">>> Data Uploaded Successfully...");      
+    }, (error) => {
+      console.log(">>> Error: ", error);
+    });
+    //this.uploadImgeToFirebase(); try it
+  }
+
+  checkImageType(event: any) {
+    this.tempImageFile = event.target.files[0];
+    if(this.tempImageFile.type === "image/jpeg" || this.tempImageFile.type === "image/jpg" || this.tempImageFile.type === "image/png") {
+
+    } else {
+      this.tempImageFile = null;
+    }
   }
 
 }
--- fsd2021angular/src/app/restaurants/restaurants.component.ts
+++ fsd2021angular/src/app/restaurants/restaurants.component.ts
@@ -1,11 +1,199 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+// import { Component, OnInit, ViewChild } from '@angular/core';
+// import { FormControl, FormGroup } from '@angular/forms';
+// import { Restaurant } from '../model/restaurant';
+// import { RestaurantsService } from '../restaurants.service';
+// import { getFirestore, collection, addDoc, getDocs, doc, Timestamp, deleteDoc, setDoc } from '@firebase/firestore/lite';
+// import { getStorage, uploadBytes, ref, getDownloadURL } from '@firebase/storage';
+// import { DBService } from '../db.service';
+// import { ActivatedRoute } from '@angular/router';
+
+// @Component({
+//   selector: 'app-restaurants',
+//   templateUrl: './restaurants.component.html',
+//   styleUrls: ['./restaurants.component.css']
+// })
+// export class RestaurantsComponent implements OnInit {
+//   /*
+//   restaurants: Restaurant[] = [
+//     new Restaurant("McDonalds", 30, 4.3, "burger, Indian, Continantal"),
+//     new Restaurant("Table By Basant", 40, 4.5, "Indian, Chinesse, Continantal"),
+//   ];
+//   */
+//   file: any;
+//   restaurantList : any;
+//   addView = false;
+//   text = "Add Restaurant";
+//   updateMode = false;
+
+//   cities = [
+//     {cityName: "Select City", state:""},
+//     {cityName: "Ludhiana", state:"Punjab", pinCode: [141001, 141002, 141005]},
+//     {cityName: "Chandigarh", state:"Punjab"},
+//     {cityName: "Amritsar", state:"Punjab"},
+//     {cityName: "Jalandhar", state:"Punjab"},
+//     {cityName: "Phagwara", state:"Punjab"},
+//   ];
+
+//   restaurantForm = new FormGroup(
+//     {
+//       name: new FormControl(''),
+//       phone: new FormControl(''),
+//       email: new FormControl(''),
+//       veg: new FormControl(false),
+//       nonVeg: new FormControl(false),
+//       servingType: new FormControl('delivery'),
+//       city: new FormControl(this.cities[0]),
+//       image: new FormControl('')
+//     }
+//   );
+
+//   //restaurants = this.service.getRestaurants();
+
+//   constructor(private db: DBService, private route: ActivatedRoute) {
+//     this.fetchPromoRestaurants();
+//    }
+
+//   action: String = "";
+//   restaurantData: any;
+
+//   ngOnInit(): void {
+//     this.route.queryParams.subscribe(params => {
+//       this.action = params['action']
+//       // if(this.action == "delete"){
+//       //   const docId = "";
+//       //   this.deleteRestaurant(docId);
+//       // } 
+//       if(this.action == "update"){
+//         this.addView = true;
+//         this.updateMode = true;
+//         this.text = "Update Restaurant";
+
+//         const sessionData = sessionStorage.getItem("restaurant");
+//         const restaurantData = JSON.parse(sessionData!);
+
+//         this.restaurantForm.patchValue(
+//           {
+//             name: this.restaurantData.name,
+//             email: this.restaurantData.email
+//           }
+//         );
+//         // const docId = "";
+//         // this.updateRestaurant(docId);
+//       }else{
+//         console.log("Do Nothing or Handle the Case");
+//       }
+//     });
+//   }
+
+//   addRestaurant(name: string, timeToDeliver: string, ratings: string, categories: string){
+//     //this.restaurants.push(new Restaurant(name, Number(timeToDeliver), Number(ratings), categories))
+//   }
+
+//   async fetchPromoRestaurants(){
+//     const firestoreDB = getFirestore(this.db.app);
+//     const promoCodeCollection = collection(firestoreDB, 'restaurants');
+//     const snapshots = await getDocs(promoCodeCollection);
+    
+//     this.restaurantList = snapshots.docs.map(
+//       doc => {
+//         const data = doc.data();
+//         data['docId'] = doc.id;
+//         return data;
+//       }
+//     );
+//     console.log(this.restaurantList);
+//   }
+
+//   pickFile(event:any){
+//     this.file = event.target.files[0];
+//     console.log(this.restaurantForm.value);
+//     console.log(this.file); 
+//   }
+
+//   uploadImgeToFirebase(){
+//     const metadata = {
+//       contentType: 'image/jpeg',
+//     };
+//     const storageReference = getStorage();
+//     const restaurantImageReference = ref(storageReference, "restaurant-images/"+this.file.name);
+//     uploadBytes(restaurantImageReference, this.file, metadata).then((snapshot) => {
+//       console.log("Image Uploaded Successfully");
+//       getDownloadURL(snapshot.ref).then((downloadURL) => {
+//         console.log('File available at', downloadURL); 
+//         // Save Restaurant Object in FirebaseFirestore
+//         const dataToSave = this.restaurantForm.value;
+//         // Save this in Firebase
+//         dataToSave['imageUrl'] = downloadURL;
+//         dataToSave['creationTime'] = Timestamp.now();
+//         console.log("dataToSave is: "+dataToSave);
+
+//         const firestoreDB = getFirestore(this.db.app);
+        
+//         const restaurantCollection = collection(firestoreDB, 'restaurants');
+//         addDoc(restaurantCollection, dataToSave);
+//         console.log("Restaurant Added");
+//        })
+//        .catch((error) => {
+//          console.log("Something Went Wrong");
+//       });
+//         });
+//   }
+
+//   deleteRestaurant(docID: any){
+//     console.log("Delete Clicked");
+//     const firestoreDB = getFirestore(this.db.app);
+//     deleteDoc(doc(firestoreDB, "restaurants", docID));
+// }
+
+// updateRestaurant(docID: any){
+//   if(this.restaurantForm.value.image != ""){
+//     // Upload the Image
+//   }
+//   const firestoreDB = getFirestore(this.db.app);
+//   const documentToWrite = doc(firestoreDB, 'restaurants', docID);
+//   const restaurantData = this.restaurantForm.value;
+//   console.log("Updating Restaurant With Data");
+//   console.log(restaurantData);
+  
+//   setDoc(documentToWrite, restaurantData);
+// }
+
+//   addRestaurantToFirebase(){
+//     if(this.updateMode){
+//       this.updateRestaurant(this.restaurantData.docId);
+//       return;
+//     }
+//     console.log(this.restaurantForm.value);
+//     this.uploadImgeToFirebase();
+//     //this.restaurantForm.reset();
+    
+//   }
+
+//   changeView(){
+//     this.addView = !this.addView;
+//     if(this.addView){
+//       this.text = "View Restaurants";
+//     }else{
+//       this.text = "Add Restaurant";
+//     }
+//   }
+
+//   saveDataInSession(restaurant: any){
+//     console.log(restaurant);
+//     sessionStorage.setItem("restaurant", JSON.stringify(restaurant));
+//     console.log("Restaurant Saved in Session Storage");
+//   }
+// }
+
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Restaurant } from '../model/restaurant';
 import { RestaurantsService } from '../restaurants.service';
-import { getFirestore, collection, addDoc, getDocs, doc, Timestamp, deleteDoc, setDoc } from '@firebase/firestore/lite';
-import { getStorage, uploadBytes, ref, getDownloadURL } from '@firebase/storage';
+import { getFirestore, collection, addDoc, getDocs, doc, Timestamp, deleteDoc, setDoc} from '@firebase/firestore/lite'
+import { getStorage, ref, uploadBytes, getDownloadURL } from '@firebase/storage'
 import { DBService } from '../db.service';
 import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-restaurants',
@@ -13,12 +201,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent implements OnInit {
-  /*
-  restaurants: Restaurant[] = [
-    new Restaurant("McDonalds", 30, 4.3, "burger, Indian, Continantal"),
-    new Restaurant("Table By Basant", 40, 4.5, "Indian, Chinesse, Continantal"),
-  ];
-  */
+
+  /*restaurants: Restaurant[] = [
+    new Restaurant("McDonalds", 30, 4.3, "burger, indian, continental"),
+    new Restaurant("Table By Basant", 40, 4.5, "indian, chinese, continental"),
+    new Restaurant("Subway", 15, 4.8, "sandwich, salads")
+  ];*/
+
   file: any;
   restaurantList : any;
   addView = false;
@@ -49,37 +238,44 @@ export class RestaurantsComponent implements OnInit {
 
   //restaurants = this.service.getRestaurants();
 
-  constructor(private db: DBService, private route: ActivatedRoute) {
-    this.fetchPromoRestaurants();
-   }
+  // if the above array is suppose to be created by fetching data from Server
+  // it will take time to load
 
+  // Injection for Service in Constructor of Component
+  //constructor(private service: RestaurantsService) { }
+  constructor(private db: DBService, private route: ActivatedRoute) { 
+    this.fetchRestaurants();
+  }
+  
   action: String = "";
   restaurantData: any;
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.action = params['action']
-      // if(this.action == "delete"){
-      //   const docId = "";
-      //   this.deleteRestaurant(docId);
-      // } 
-      if(this.action == "update"){
+     if(this.action == "update"){  // Update Here
         this.addView = true;
         this.updateMode = true;
         this.text = "Update Restaurant";
 
         const sessionData = sessionStorage.getItem("restaurant");
-        const restaurantData = JSON.parse(sessionData!);
+        this.restaurantData = JSON.parse(sessionData!);
 
         this.restaurantForm.patchValue(
           {
             name: this.restaurantData.name,
-            email: this.restaurantData.email
+            phone: this.restaurantData.phone,
+            email: this.restaurantData.email,
+            veg: this.restaurantData.veg,
+            nonVeg: this.restaurantData.nonVeg,
+            servingType: this.restaurantData.servingType,
+            city: this.restaurantData.city
           }
         );
-        // const docId = "";
-        // this.updateRestaurant(docId);
+        
       }else{
+        this.updateMode = false;
+        this.text = "Add Restaurant";
         console.log("Do Nothing or Handle the Case");
       }
     });
@@ -89,37 +285,43 @@ export class RestaurantsComponent implements OnInit {
     //this.restaurants.push(new Restaurant(name, Number(timeToDeliver), Number(ratings), categories))
   }
 
-  async fetchPromoRestaurants(){
+  async fetchRestaurants(){
     const firestoreDB = getFirestore(this.db.app);
     const promoCodeCollection = collection(firestoreDB, 'restaurants');
     const snapshots = await getDocs(promoCodeCollection);
     
     this.restaurantList = snapshots.docs.map(
       doc => {
-        const data = doc.data();
-        data['docID'] = doc.id;
-        return data;
+          const data = doc.data();
+          data['docId'] = doc.id;
+          return data;
       }
     );
+     
     console.log(this.restaurantList);
+
+    // snapshots.docs.map(
+    //   doc => console.log(doc.id)
+    // );
+
   }
 
   pickFile(event:any){
     this.file = event.target.files[0];
     console.log(this.restaurantForm.value);
-    console.log(this.file); 
+    console.log(this.file);
   }
 
   uploadImgeToFirebase(){
-    const metadata = {
-      contentType: 'image/jpeg',
-    };
-    const storageReference = getStorage();
-    const restaurantImageReference = ref(storageReference, "restaurant-images/"+this.file.name);
-    uploadBytes(restaurantImageReference, this.file, metadata).then((snapshot) => {
-      console.log("Image Uploaded Successfully");
-      getDownloadURL(snapshot.ref).then((downloadURL) => {
-        console.log('File available at', downloadURL); 
+      const metadata = {
+        contentType: 'image/png',
+      };
+      const storageReference = getStorage();
+      const restaurantImageReference = ref(storageReference, "restaurant-images/"+this.file.name);
+      uploadBytes(restaurantImageReference, this.file, metadata).then((snapshot) => {
+       console.log("Image Uploaded Successfully");
+       getDownloadURL(snapshot.ref).then((downloadURL) => {
+        console.log('File available at', downloadURL);
         // Save Restaurant Object in FirebaseFirestore
         const dataToSave = this.restaurantForm.value;
         // Save this in Firebase
@@ -132,46 +334,51 @@ export class RestaurantsComponent implements OnInit {
         const restaurantCollection = collection(firestoreDB, 'restaurants');
         addDoc(restaurantCollection, dataToSave);
         console.log("Restaurant Added");
-       })
-       .catch((error) => {
-         console.log("Something Went Wrong");
+        
+    })
+    .catch((error) =>{
+      console.log("Something Went Wrong");
+    }); 
       });
-        });
   }
-
+  
   deleteRestaurant(docID: any){
-    console.log("Delete Clicked");
+      console.log("Delete Clicked");
     const firestoreDB = getFirestore(this.db.app);
     deleteDoc(doc(firestoreDB, "restaurants", docID));
-}
-
-updateRestaurant(docID: any){
-  if(this.restaurantForm.value.image != ""){
-    // Upload the Image
   }
-  const firestoreDB = getFirestore(this.db.app);
-  const documentToWrite = doc(firestoreDB, 'restaurants', docID);
-  const restaurantData = this.restaurantForm.value;
-  console.log("Updating Restaurant With Data");
-  console.log(restaurantData);
-  
-  setDoc(documentToWrite, restaurantData);
-}
+
+  updateRestaurant(docID: any){
+
+    if(this.restaurantForm.value.image != ""){
+      // Upload the Image
+    }
+
+    const firestoreDB = getFirestore(this.db.app);
+    const documentToWrite = doc(firestoreDB, 'restaurants', docID);
+    const restaurantData = this.restaurantForm.value;
+    console.log("Updating Restaurant with Data:");
+    console.log(restaurantData);
+    
+    
+    setDoc(documentToWrite, restaurantData);
+  }
+    
 
   addRestaurantToFirebase(){
+
     if(this.updateMode){
-      this.updateRestaurant(this.restaurantData.docID);
+      this.updateRestaurant(this.restaurantData.docId);
       return;
     }
+
     console.log(this.restaurantForm.value);
     this.uploadImgeToFirebase();
-    //this.restaurantForm.reset();
-    
   }
 
   changeView(){
     this.addView = !this.addView;
-    if(this.addView){
+    if(!this.addView){
       this.text = "View Restaurants";
     }else{
       this.text = "Add Restaurant";
@@ -183,6 +390,6 @@ updateRestaurant(docID: any){
     sessionStorage.setItem("restaurant", JSON.stringify(restaurant));
     console.log("Restaurant Saved in Session Storage");
   }
-}
 
+}
 
